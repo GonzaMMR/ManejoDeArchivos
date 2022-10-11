4aa9f95831b13d7526e359c08006cdd4cadcff2b
GonzaMMR <gonzalomoratomartin@gmail.com>
2022-10-11 10:08:11 +0200

    cambios

---
+++ ManejoDeArchivos/src/manejoarchivos/ManejoDeArchivos.java
@@ -0,0 +1,16 @@
+/*
+*   NUESTRA CLASE MANEJO DE ARCHIVOS, VA A CONTENER LOS SIGUIENTES PUNTOS:
+       - Como crear un archivo
+       - Guardar la informacion en un archivo
+       - Eliminar la informacion de un archivo
+       - Eliminar un archivo
+ */
+package manejoarchivos;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class ManejoDeArchivos {
+    
+}
